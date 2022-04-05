d4ef2474be03ac8852bc2249b1f2c56cd37af2f7
Aryan Verma <102582703+aryanverma2506@users.noreply.github.com>
2022-04-05 22:32:53 +0530

    Changes Done in Admin Pannel

--- public/js/admin.js
+++ public/js/admin.js
@@ -11,76 +11,75 @@ const lightModeClass = "light-mode";
 
 /*TOGGLE HEADER STATE*/
 collapseBtn.addEventListener("click", function () {
-  body.classList.toggle(collapsedClass);
-  this.getAttribute("aria-expanded") == "true"
+    body.classList.toggle(collapsedClass);
+    this.getAttribute("aria-expanded") == "true"
     ? (this.setAttribute("aria-expanded", "false"),
-      document.querySelector(".admin-menu img").style.display = "none",
-      document.querySelector(".admin-menu").style.marginTop = (35).toString()+"px")
-      : (this.setAttribute("aria-expanded", "true"),
-      document.querySelector(".admin-menu img").style.display = "initial",
-      document.querySelector(".admin-menu").style.marginTop = 0);
-  this.getAttribute("aria-label") == "collapse menu"
+        document.querySelector(".admin-menu img").style.display = "none",
+        document.querySelector(".admin-menu").style.marginTop = (35).toString()+"px")
+    : (this.setAttribute("aria-expanded", "true"),
+        document.querySelector(".admin-menu img").style.display = "initial",
+        document.querySelector(".admin-menu").style.marginTop = 0);
+    this.getAttribute("aria-label") == "collapse menu"
     ? this.setAttribute("aria-label", "expand menu")
     : this.setAttribute("aria-label", "collapse menu");
 });
 
 /*TOGGLE MOBILE MENU*/
 toggleMobileMenu.addEventListener("click", function () {
-  body.classList.toggle("mob-menu-opened");
-  this.getAttribute("aria-expanded") == "true"
+    body.classList.toggle("mob-menu-opened");
+    this.getAttribute("aria-expanded") == "true"
     ? (this.setAttribute("aria-expanded", "false"),
-      document.querySelector(".admin-menu").classList.remove("active"))
+        document.querySelector(".admin-menu").classList.remove("active"))
     : (this.setAttribute("aria-expanded", "true"),
-      document.querySelector(".admin-menu").classList.add("active"),
-      document.querySelector(".admin-menu img").style.display = "initial",
-      document.querySelector(".admin-menu").style.marginTop = 0);
-  this.getAttribute("aria-label") == "open menu"
+        document.querySelector(".admin-menu").classList.add("active"),
+        document.querySelector(".admin-menu img").style.display = "initial",
+        document.querySelector(".admin-menu").style.marginTop = 0);
+    this.getAttribute("aria-label") == "open menu"
     ? this.setAttribute("aria-label", "close menu")
     : this.setAttribute("aria-label", "open menu");
 });
 
 /*SHOW TOOLTIP ON MENU LINK HOVER*/
 for (const link of menuLinks) {
-  link.addEventListener("mouseenter", function () {
-    if (
-      body.classList.contains(collapsedClass) &&
-      window.matchMedia("(min-width: 768px)").matches
-    ) {
-      const tooltip = this.querySelector("span").textContent;
-      this.setAttribute("title", tooltip);
-    } else {
-      this.removeAttribute("title");
+    link.addEventListener("mouseenter", function () {
+    if (body.classList.contains(collapsedClass) && window.matchMedia("(min-width: 768px)").matches) {
+        const tooltip = this.querySelector("span").textContent;
+        this.setAttribute("title", tooltip);
     }
-  });
+    else {
+        this.removeAttribute("title");
+    }
+    });
 }
 
 window.addEventListener('resize', () => {
-  window.innerWidth > 767 
-  ? (body.classList.toggle("mob-menu-opened"),
-    toggleMobileMenu.setAttribute("aria-expanded", "false"),
-    document.querySelector(".admin-menu").classList.remove("active"),
-    toggleMobileMenu.setAttribute("aria-label", "close menu"))
-  : (body.classList.remove(collapsedClass),
-    collapseBtn.setAttribute("aria-expanded", "true"),
-    document.querySelector(".admin-menu img").style.display = "initial",
-    document.querySelector(".admin-menu").style.marginTop = 0,
-    collapseBtn.setAttribute("aria-label", "expand menu"));
+    window.innerWidth > 767 
+    ? (body.classList.remove("mob-menu-opened"),
+        toggleMobileMenu.setAttribute("aria-expanded", "false"),
+        document.querySelector(".admin-menu").classList.remove("active"),
+        toggleMobileMenu.setAttribute("aria-label", "close menu"))
+    : (body.classList.remove(collapsedClass),
+        collapseBtn.setAttribute("aria-expanded", "true"),
+        document.querySelector(".admin-menu img").style.display = "initial",
+        document.querySelector(".admin-menu").style.marginTop = 0,
+        collapseBtn.setAttribute("aria-label", "expand menu"));
 });
 
 /*TOGGLE LIGHT/DARK MODE*/
 // if (localStorage.getItem("dark-mode") === "false") {
-//   html.classList.add(lightModeClass);
-//   switchInput.checked = false;
-//   switchLabelText.textContent = "Light";
+//     html.classList.add(lightModeClass);
+//     switchInput.checked = false;
+//     switchLabelText.textContent = "Light";
 // }
 
 // switchInput.addEventListener("input", function () {
-//   html.classList.toggle(lightModeClass);
-//   if (html.classList.contains(lightModeClass)) {
-//     switchLabelText.textContent = "Light";
-//     localStorage.setItem("dark-mode", "false");
-//   } else {
-//     switchLabelText.textContent = "Dark";
-//     localStorage.setItem("dark-mode", "true");
-//   }
+//     html.classList.toggle(lightModeClass);
+//     if (html.classList.contains(lightModeClass)) {
+//         switchLabelText.textContent = "Light";
+//         localStorage.setItem("dark-mode", "false");
+//     }
+//     else {
+//         switchLabelText.textContent = "Dark";
+//         localStorage.setItem("dark-mode", "true");
+//     }
 // });
